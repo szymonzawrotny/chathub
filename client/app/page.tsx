396885e9de9d65dc07f06dff3f7abaa8d083d64d
szymonzawrotny <szymonzawrotny@gmail.com>
2026-05-24 23:03:49 +0200

    add blobs to the login page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,29 @@
 import './page.style.scss';
+import Image from 'next/image';
+import Blob from "../images/blob.svg"
 
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <div className="leftSide">
+        <Image
+          src={Blob}
+          alt="blob"
+          width={400}
+          height={400}
+          className="blobTop"
+        />
+        <Image
+          src={Blob}
+          alt="blob"
+          width={400}
+          height={400}
+          className="blobBottom"
+        />
+      </div>
+      <div className="rightSide"></div>
+    </div>
+  );
 };
 
 export default App;
