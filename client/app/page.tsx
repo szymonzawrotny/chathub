514777d29b8a862fb2a35eafda30ea553834975b
szymonzawrotny <szymonzawrotny@gmail.com>
2026-05-25 22:46:03 +0200

    add cool blob shadow

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,25 +1,17 @@
 import './page.style.scss';
 import Image from 'next/image';
-import Blob from "../images/blob.svg"
+import Blob from '../images/blob.svg';
 
 const App = () => {
   return (
     <div className="app">
       <div className="leftSide">
-        <Image
-          src={Blob}
-          alt="blob"
-          width={400}
-          height={400}
-          className="blobTop"
-        />
-        <Image
-          src={Blob}
-          alt="blob"
-          width={400}
-          height={400}
-          className="blobBottom"
-        />
+        <div className="blobTop">
+          <Image src={Blob} alt="blob" width={400} height={400} />
+        </div>
+        <div className="blobBottom">
+          <Image src={Blob} alt="blob" width={400} height={400} />
+        </div>
       </div>
       <div className="rightSide"></div>
     </div>
